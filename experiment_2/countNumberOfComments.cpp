7d1fe774d877662a4d275ed058c45956ffc669e4
K Om Senapati <komnoob123@gmail.com>
2026-01-30 13:17:04 +0000

    exp2

---
+++ experiment_2/countNumberOfComments.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    string filepath = "/workspaces/CD_LAB/experiment_2/in.c";
+    ifstream file(filepath);
+    if (!file.is_open())
+    {
+        cerr << "Error: Unable to open the file" << endl;
+        return 1;
+    }
+
+    int comments = 0;
+    bool inBlock = false;
+    char prev = 0, cur;
+
+    while (file.get(cur))
+    {
+        if (!inBlock && prev == '/' && cur == '/')
+        {
+            comments++;
+            while (file.get(cur) && cur != '\n')
+                ;
+            prev = 0;
+            continue;
+        }
+
+        if (!inBlock && prev == '/' && cur == '*')
+        {
+            comments++;
+            inBlock = true;
+            prev = 0;
+            continue;
+        }
+
+        if (inBlock && prev == '*' && cur == '/')
+        {
+            inBlock = false;
+            prev = 0;
+            continue;
+        }
+
+        prev = cur;
+    }
+
+    cout << "Number of comments: " << comments << endl;
+
+    return 0;
+}
---
+++ experiment_2/in.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+/* main function */
+int main() {
+    // print hello world
+    printf("Hello, World!\n"); // printing
+    return 0;
+}
